524907f2f872321916dd958ca68cc665ba969af3
mugihouse <mugi.stnk100122@gmail.com>
2024-11-28 15:50:29 +0900

    Add: BooksController作成

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    public function index() {}
+
+    public function store() {}
+
+    public function edit() {}
+
+    public function update() {}
+
+    public function destroy() {}
+}
