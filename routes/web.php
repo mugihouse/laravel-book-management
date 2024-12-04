fded3464f4e8424d744c5ba9b48f73dd42da04b4
mugihouse <mugi.stnk100122@gmail.com>
2024-12-04 11:53:35 +0900

    Add: 本管理のルーティングを作成

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BooksController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::prefix('books')->middleware(['auth'])
+    ->controller(BooksController::class)
+    ->name('books.')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::post('/', 'store')->name('store');
+        Route::delete('{id}/delete', 'destroy')->name('destroy');
+    });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,4 +35,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
