4dd7c53006a6e1cec3c5f79c09adc7f2418a0030
mugihouse <mugi.stnk100122@gmail.com>
2024-12-07 15:16:35 +0900

    Add: 本登録フォームに出版日の項目を追加

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -35,6 +35,12 @@
                                 </div>
                               </div>
                               <div class="p-2 w-full">
+                                <div class="relative">
+                                  <label for="published" class="leading-7 text-sm text-gray-600">出版日</label>
+                                  <input type="date" id="published" name="published" value="{{ old('published') }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                              </div>
+                              <div class="p-2 w-full">
                                 <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
                               </div>
                             </div>
