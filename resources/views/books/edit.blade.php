31651650a48c11f49efd554da533bef7fcbe96d5
mugihouse <mugi.stnk100122@gmail.com>
2024-12-07 13:35:31 +0900

    Add: 編集フォームを追加

---
+++ resources/views/books/edit.blade.php
@@ -0,0 +1,56 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            編集画面
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                  <x-input-error :messages="$errors->all()" class="mt-2" />
+                    <section class="text-gray-600 body-font relative">
+                      <form method="post" action="{{ route('books.update', [ 'id' => $book->id ])}}">
+                        @csrf
+                        <div class="container px-5 mx-auto">
+                          <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                            <div class="flex flex-col -m-2">
+                              <div class="p-2 w-full">
+                                <div class="relative">
+                                  <label for="item_name" class="leading-7 text-sm text-gray-600">本のタイトル</label>
+                                  <input type="text" id="item_name" name="item_name" value="{{ $book->item_name }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                              </div>
+                              <div class="p-2 w-full">
+                                <div class="relative">
+                                  <label for="item_number" class="leading-7 text-sm text-gray-600">冊数</label>
+                                  <input type="number" id="item_number" name="item_number" value="{{ $book->item_number }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                              </div>
+                              <div class="p-2 w-full">
+                                <div class="relative">
+                                  <label for="item_amount" class="leading-7 text-sm text-gray-600">価格</label>
+                                  <input type="number" id="item_amount" name="item_amount" value="{{ $book->item_amount }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                              </div>
+                              <div class="p-2 w-full">
+                                <div class="relative">
+                                  <label for="published" class="leading-7 text-sm text-gray-600">出版日</label>
+                                  <input type="date" id="published" name="published" value="{{ $book->published }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                              </div>
+                              <div class="p-2 w-full">
+                                <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Save
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
